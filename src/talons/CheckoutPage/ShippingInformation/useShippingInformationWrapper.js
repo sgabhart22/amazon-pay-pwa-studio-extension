95cce399da98e5a75100c3c32de34d8e928c4026
Spencer Gabhart <sgabhart2230@gmail.com>
2022-07-14 13:03:05 -0400

    Shipping information edit bind change action corrected

--- src/talons/CheckoutPage/ShippingInformation/useShippingInformationWrapper.js
+++ src/talons/CheckoutPage/ShippingInformation/useShippingInformationWrapper.js
@@ -1,19 +1,40 @@
+import { useEffect } from "react";
+import useScript from '@magento/peregrine/lib/hooks/useScript';
+
 const wrapUseShippingInformation = original => {
     return function useShippingInformation(props) {
         const { 
             handleEditShipping,
+            isLoading,
             ...defaults 
         } = original(props);
         
-        const isAmazonCheckout = JSON.parse(localStorage.getItem('amazon-checkout-session'))?.id;
+        const checkoutSessionId = JSON.parse(localStorage.getItem('amazon-checkout-session'))?.id
+        const isAmazonCheckout = checkoutSessionId;
+        const amznScriptStatus = useScript("https://static-na.payments-amazon.com/checkout.js");
         
         var myHandleEditShipping = handleEditShipping;
         if (isAmazonCheckout) {
             myHandleEditShipping = () => {};
         }
 
+        useEffect(() => {
+            if (!isLoading) {
+                if (isAmazonCheckout && amznScriptStatus === 'ready' ) {
+                    const editSelector = '.shippingInformation-editButton-8ST';
+                    if (document.querySelectorAll(editSelector).length) {
+                        window.amazon.Pay.bindChangeAction(editSelector, {
+                            amazonCheckoutSessionId: checkoutSessionId,
+                            changeAction: 'changeAddress'
+                        });
+                    }
+                }
+            }
+        });
+
         return {
             handleEditShipping: myHandleEditShipping,
+            isLoading,
             ...defaults
         };
     };
--- src/talons/CheckoutPage/useCheckoutPage.js
+++ src/talons/CheckoutPage/useCheckoutPage.js
@@ -1,18 +1,15 @@
 import { useCallback, useEffect } from 'react';
 import { useCartContext } from '@magento/peregrine/lib/context/cart';
 import { useUpdateCheckoutSession } from '../AmazonCheckoutSession/useUpdateCheckoutSession';
-import useScript from '@magento/peregrine/lib/hooks/useScript';
 
 const wrapUseCheckoutPage = original => {
     return function useCheckoutPage(props) {
         const { 
             handlePlaceOrder,
-            isLoading,
             ...defaults 
         } = original(props);
         
         const [{cartId}] = useCartContext();
-        const amznScriptStatus = useScript("https://static-na.payments-amazon.com/checkout.js");
         
         const checkoutSessionId = JSON.parse(localStorage.getItem('amazon-checkout-session'))?.id; 
         const isAmazonCheckout = checkoutSessionId;
@@ -41,21 +38,8 @@ const wrapUseCheckoutPage = original => {
             }
         }, [updateLoading, updateData]);
 
-        useEffect(() => {
-            if (isAmazonCheckout && amznScriptStatus === 'ready' && !isLoading) {
-                const editSelector = '.shippingInformation-editButton-8ST';
-                if (document.querySelectorAll(editSelector).length) {
-                    window.amazon.Pay.bindChangeAction(editSelector, {
-                        amazonCheckoutSessionId: checkoutSessionId,
-                        changeAction: 'changeAddress'
-                    });
-                }
-            }
-        }, [amznScriptStatus, isLoading]);
-
         return {
             handlePlaceOrder: myHandlePlaceOrder,
-            isLoading,
             ...defaults
         };
     };
